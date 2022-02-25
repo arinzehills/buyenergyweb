ae62172384a992446b6fc9bb4ed276b416cfbe01
arinzehills <arinzehill@gmail.com>
2022-02-25 17:48:21 +0100

    updates

--- src/components/Uniqueness/Uniqueness.jsx
+++ src/components/Uniqueness/Uniqueness.jsx
@@ -3,13 +3,19 @@ import './Uniqueness.css';
 
 const imgs=['images/power.png','images/money.png','images/secure.png']
 const heading=['Buy Energy Units','Ofline Purchase','Fast and secure'];
-const desc=['Buy energy gives you the freedom tobuy energy units'+
-'in an easier,better, smarter and more secure way from'+
-'from the conformt of your home','Buy energy gives you the freedom tobuy energy units'+
-'in an easier,better, smarter and more secure way from'+
-'from the conformt of your home,Buy energy gives you the freedom tobuy energy units'+
-'in an easier,better, smarter and more secure way from'+
-'from the conformt of your home']
+const desc=[
+    'With buy energy you can purchase prepaid meter tokens at just a click'+
+    ' and get your tokens immediately, you can also pay for post paid meters '+
+    'in an easier,better, and more smarter smarter way from the comfort of your home',
+
+    "Don't have interner connection? Don't worry Buy Energy made it possible to purchase tokens "+
+    ' for your meters by using ussd code'+
+    'from the anywhere',
+
+    'With Buy energy fast and more secure way has been provided to buy tokens'+
+     "You can trust us for fast and safe transactions"
+    
+]
 function Uniqueness() {
    
   return <>
@@ -28,14 +34,14 @@ function Uniqueness() {
                     <img src={imgs[1]} className='img' alt=''/>
                         <h3  className='unique_heading2 '>{heading[1]}</h3>
                         <div className="card-info">
-                        <p>{desc[0]}</p>
+                        <p>{desc[1]}</p>
                         </div>
                     </div> 
                     <div className="unique-card">
                     <img src={imgs[2]}className='img' alt=''/>
                         <div className="card-info">
                         <h3  className='unique_heading2 '>{heading[2]}</h3>
-                        <p>{desc[0]}</p>
+                        <p>{desc[2]}</p>
                         </div>
                     </div>
                  </div>
--- src/pages/Homepage/Data.js
+++ src/pages/Homepage/Data.js
@@ -13,9 +13,9 @@ export const homeData={
 }
 export const homeData2={
     headline:'Paying bills in a faster and more secure way',
-    description: 'Buy energy gives you the freedom to buy energy units '+
-                   'in an easier,better, smarter and more secure way with your debit card'+
-                   ' from and other utilities with debit card and various payment methods',
+    description: 'Paying electricity bills and buying energy tokens is made faster, than '+
+                   "than you think, get a token at just a click, you don't have to worry about "+
+                   'we use secure payment gateway',
     showButton: false,
     // buttonLabel:['Sign up','Buy units'],
     img:'images/cardslant.png',
@@ -25,9 +25,9 @@ export const homeData2={
 }
 export const homeData3={
     headline:'Get our mobile app today',
-    description: 'Buy energy gives you the freedom tobuy energy units'+
-                   'in an easier,better, smarter and more secure way '+
-                   'from the confort of your home',
+    description: 'Our mobile app makes it easier for you to buy units without'+
+                   ' having the think of our website url and downloading our apps gives '+
+                   'you an opportunity for better offers and rewards. Get our app today!',
     showButton: true,
     buttonLabel:['Google play','App store'],
     smallImage:'images/twocircle.png',

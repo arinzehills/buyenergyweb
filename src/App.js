7cbcb9b83ca243cc38e18f7dc1c5f1d58a045402
arinzehills <arinzehill@gmail.com>
2022-02-28 09:29:51 +0100

    updates

--- src/App.js
+++ src/App.js
@@ -42,7 +42,7 @@ const App = () => {
         setLoading(true)
         setTimeout(()=>{
             setLoading(false)
-        },3000)
+        },1000)
     },[])
   return (
          <div>                      
